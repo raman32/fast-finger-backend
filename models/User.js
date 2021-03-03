d38675ba8b97d180a8d74c696b2ec72dd75dba09
raman32 <rg.raman.ghimire@gmail.com>
2021-03-03 15:22:36 +0545

    added port to mysql server config

--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
