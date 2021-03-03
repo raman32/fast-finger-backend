874ee668ad7dedb03257f9086968f4859b716a96
raman32 <rg.raman.ghimire@gmail.com>
2021-03-03 11:38:07 +0545

    fixed import issue of dotenv in production mode

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
-const app = express()
-require('dotenv').config();
+const app = express();
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+  }
 const port = process.env.PORT
 
 
